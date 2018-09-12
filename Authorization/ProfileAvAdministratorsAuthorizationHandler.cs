6c983b39eaf5c0fde7c811c169772c0603990859
andrewroux <andrewjroux@gmail.com>
2018-09-12 12:47:15 +0100

    Added Authorisation Files from .Net

---
+++ Authorization/ProfileAvAdministratorsAuthorizationHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using EasyMind.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace EasyMind.Authorization
+{
+    public class ProfileAvAdministratorsAuthorizationHandler
+                    : AuthorizationHandler<OperationAuthorizationRequirement, ProfileAvailability>
+    {
+        protected override Task HandleRequirementAsync(
+                                              AuthorizationHandlerContext context,
+                                    OperationAuthorizationRequirement requirement,
+                                     ProfileAvailability resource)
+        {
+            if (context.User == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            // Administrators can do anything.
+            if (context.User.IsInRole(Constants.ContactAdministratorsRole))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
